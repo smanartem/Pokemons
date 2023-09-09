26fc67ccad8c8b5f01e3cadb135879dab2814402
Artsem-Barysau <artem.borisov@innowise-group.com>
2023-09-10 00:38:51 +0200

    start the project

---
+++ app/src/main/java/com/example/pokemons/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pokemons
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
